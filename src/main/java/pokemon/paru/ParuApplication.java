c4ea79904e4ce67de5e5b6a6d97a7e93597961ff
e1b22078 <e1b22078@oit.ac.jp>
2024-10-22 14:04:20 +0900

    初期化

---
+++ src/main/java/pokemon/paru/ParuApplication.java
@@ -0,0 +1,13 @@
+package pokemon.paru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pokemon/paru/ParuApplicationTests.java
@@ -0,0 +1,13 @@
+package pokemon.paru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
