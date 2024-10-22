d45b2244be7e1737cb5f494448a934b989644379
e1b22090 <e1b22090.oit.ac.jp>
2024-10-22 15:06:02 +0900

    T10まで

--- src/main/java/pokemon/paru/security/Sample3AuthConfiguration.java
+++ src/main/java/pokemon/paru/security/Sample3AuthConfiguration.java
@@ -28,13 +28,7 @@ public class Sample3AuthConfiguration {
             .logoutUrl("/logout")
             .logoutSuccessUrl("/")) // ログアウト後に / にリダイレクト
         .authorizeHttpRequests(authz -> authz
-            .requestMatchers(AntPathRequestMatcher.antMatcher("/sample3/**"))
-            .authenticated() // /sample3/以下は認証済みであること
-            .requestMatchers(AntPathRequestMatcher.antMatcher("/sample4/**"))
-            .authenticated() // /sample4/以下は認証済みであること
             .requestMatchers(AntPathRequestMatcher.antMatcher("/sample5/**"))
-            .authenticated() // /sample4/以下は認証済みであること
-            .requestMatchers(AntPathRequestMatcher.antMatcher("/sample58*"))
             .authenticated() // /sample4/以下は認証済みであること
             .requestMatchers(AntPathRequestMatcher.antMatcher("/**"))
             .permitAll())// 上記以外は全員アクセス可能
@@ -66,18 +60,18 @@ public class Sample3AuthConfiguration {
     UserDetails admin = User.withUsername("admin")
         .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e").roles("ADMIN").build();
     // $ sshrun htpasswd -nbBC 10 customer1 p@ss
-    UserDetails customer1 = User.withUsername("customer1")
-        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+    UserDetails customer1 = User.withUsername("森内")
+        .password("{bcrypt}$2y$05$vA1XJ1vrS3T7pTIW6R53eeTUfkOIRGfrQy7a70jrsM95EMQauKWem")
         .roles("CUSTOMER")
         .build();
     // $ sshrun htpasswd -nbBC 10 customer2 p@ss
-    UserDetails customer2 = User.withUsername("customer2")
-        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+    UserDetails customer2 = User.withUsername("前田")
+        .password("{bcrypt}$2y$05$K5fv7QISdcx57ve98ON/Uu3oIMlI93YIE6tf8An/01c5HGsOKEQIy")
         .roles("CUSTOMER")
         .build();
     // $ sshrun htpasswd -nbBC 10 seller p@ss
-    UserDetails seller = User.withUsername("seller")
-        .password("{bcrypt}$2y$10$ngxCDmuVK1TaGchiYQfJ1OAKkd64IH6skGsNw1sLabrTICOHPxC0e")
+    UserDetails seller = User.withUsername("大川")
+        .password("{bcrypt}$2y$05$vxNpJlan5A9EQlYEdTHksukO5x0RcjVxX7FYahXGDnop72X5NyxLG")
         .roles("SELLER")
         .build();
 
